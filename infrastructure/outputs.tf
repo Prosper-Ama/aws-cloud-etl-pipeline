326a8234c0be2e0c2e7e49895b73f8f0d2900aba
Prosper-Ama <amaprosperjr@gmail.com>
2025-07-10 13:27:48 -0700

    Initial commit

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -7,8 +7,10 @@ output "s3_bucket_arn" {
   value = aws_s3_bucket.etl_bucket.arn
 }
 output "s3_bucket_versioning_status" {
-  value = aws_s3_bucket_versioning.etl_versioning.status
+  description = "The versioning status of the ETL S3 bucket."
+  value       = aws_s3_bucket.etl_bucket.versioning[0].status
 }
 output "s3_bucket_encryption" {
-  value = aws_s3_bucket_server_side_encryption_configuration.sse_config.rule[0].apply_server_side_encryption_by_default[0].sse_algorithm
+  description = "The SSE algorithm used for the ETL S3 bucket."
+  value       = one(aws_s3_bucket_server_side_encryption_configuration.sse_config.rule[*].apply_server_side_encryption_by_default[0].sse_algorithm)
 }
